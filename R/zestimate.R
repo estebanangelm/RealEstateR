3c907501cbc089d699b1f32749d4df1139d52a0e
Jill Cates <cates.jill@gmail.com>
2018-04-03 15:32:23 -0700

    Add documentation for get zpid function

--- R/zestimate.R
+++ R/zestimate.R
@@ -1,12 +1,20 @@
-# Get Zestimate
-# 
-# Example: 
-# address <- '2144+Bigelow+Ave'
-# citystatezip <- 'Seattle%2C+WA'
-# get_zpid(zwsid, address, citystatezip)
-# zpid <- get_zpid(zwsid, address, citystatezip)
-# get_zestimate_zpid <- function(zwsid, zpid)
-
+#' Get Search Results Response
+get_search_results <- function(zwsid, address, citystatezip) {
+  url_search <- 'http://www.zillow.com/webservice/GetSearchResults.htm?'
+  result <- httr::GET(url = paste0(url_search, 'zws-id=', zwsid, '&address=', address, '&citystatezip=', citystatezip))
+}
+  
+#' Get Zillow Property ID
+#' 
+#' @description 
+#' Retrieves Zillow Property ID (zpid) from the GetSearchResults API.
+#' 
+#' @param zwsid Access token 
+#' 
+#' @param address Street address of interest as a string (e.g., '2144+Bigelow+Ave')
+#' 
+#' @param citystatezip either the city and state or ZIP code of the street address (e.g., 'Seattle%2C+WA')
+#' @export
 get_zpid <- function(zwsid, address, citystatezip) {
   url_search <- 'http://www.zillow.com/webservice/GetSearchResults.htm?'
   result <- httr::GET(url = paste0(url_search, 'zws-id=', zwsid, '&address=', address, '&citystatezip=', citystatezip))
@@ -23,6 +31,16 @@ get_zestimate <- function(zwsid, zpid) {
   currency <- xml2::xml_attr(xml_find_all(zillow_xml, ".//zestimate/amount"), "currency")
   return(zestimate)
 }
+
+
+# Get Zestimate
+# 
+# Example: 
+# address <- '2144+Bigelow+Ave'
+# citystatezip <- 'Seattle%2C+WA'
+# get_zpid(zwsid, address, citystatezip)
+# zpid <- get_zpid(zwsid, address, citystatezip)
+# get_zestimate_zpid <- function(zwsid, zpid)
 
 
 
