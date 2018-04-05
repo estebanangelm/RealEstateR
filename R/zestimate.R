3004ea7985152d4883713baba2b7aeb5ccd67d13
Jill Cates <cates.jill@gmail.com>
2018-04-04 20:55:17 -0700

    Add renviron to gitignore, write get_zpid() test

--- R/zestimate.R
+++ R/zestimate.R
@@ -35,8 +35,8 @@ get_search_results <- function(zwsid, address, citystatezip) {
 get_zpid <- function(zwsid, address, citystatezip) {
   url_search <- 'http://www.zillow.com/webservice/GetSearchResults.htm?'
   result <- httr::GET(url = paste0(url_search, 'zws-id=', zwsid, '&address=', address, '&citystatezip=', citystatezip))
-  search_xml <- xml2::read_xml(content(result, "text"))
-  zpid <- xml2::xml_text(xml_find_all(search_xml, ".//zpid"))
+  search_xml <- xml2::read_xml(httr::content(result, "text"))
+  zpid <- xml2::xml_text(xml2::xml_find_all(search_xml, ".//zpid"))
   return(zpid)
 }
 
@@ -54,8 +54,8 @@ get_zestimate <- function(zwsid, zpid) {
   url_zillow <- 'http://www.zillow.com/webservice/GetZestimate.htm?'
   result <- httr::GET(url = paste0(url_zillow,'zws-id=', zwsid, '&zpid=', zpid))
   zillow_xml <- xml2::read_xml(content(result, "text"))
-  zestimate <- xml2::xml_text(xml_find_all(zillow_xml, ".//zestimate/amount"))
-  currency <- xml2::xml_attr(xml_find_all(zillow_xml, ".//zestimate/amount"), "currency")
+  zestimate <- xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//zestimate/amount"))
+  currency <- xml2::xml_attr(xml2::xml_find_all(zillow_xml, ".//zestimate/amount"), "currency")
   return(zestimate)
 }
 
---
+++ tests/testthat/test_zestimate.R
@@ -0,0 +1,26 @@
+context("zestimate.R")
+
+# -----------------------------------------------------------------------------
+# Setup
+# -----------------------------------------------------------------------------
+
+zwsid <- function() {
+  val <- Sys.getenv("ZWSID")
+  if (identical(val, "")) {
+    stop("`ZWSID` env var has not been set")
+  }
+  val
+}
+
+zwsid <- zwsid()
+
+# -----------------------------------------------------------------------------
+# get_zpid()
+# -----------------------------------------------------------------------------
+
+test_that("get_zpid() generates appropriate zpid", {
+  address <- '2144+Bigelow+Ave'
+  citystatezip <- 'Seattle%2C+WA'
+  output <- get_zpid(zwsid, address, citystatezip)
+  expect_equal(output, '48879021')
+})
