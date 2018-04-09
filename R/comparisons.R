eec013941aaae6f34d6531e258dd863fb9c7b955
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-04-08 22:42:36 -0700

    Extract data for prices

--- R/comparisons.R
+++ R/comparisons.R
@@ -45,21 +45,17 @@ get_comp_df <- function(zpid,count){
 #' @export
 
 
-get_comp_df <- function(zpid){
+price_plot <- function(zpid){
   zwsid <- getOption("ZWSID")
   url_zillow <- 'http://www.zillow.com/webservice/GetDeepComps.htm?'
   url_zillow <- paste0(url_zillow,'zws-id=', zwsid, '&zpid=', zpid,'&count=25&rentzestimate=true')
   result <- httr::GET(url_zillow)
   zillow_xml <- xml2::read_xml(httr::content(result, "text"))
 
-  zpid <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/zpid")))
-  bedrooms <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/bedrooms")))
-  bathrooms <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/bathrooms")))
-  year <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/yearBuilt")))
-  size <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/finishedSqFt")))
-  lot_size <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/lotSizeSqFt")))
-  value <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/zestimate/amount")))
-  rent <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/rentzestimate/amount")))
+  price_low <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/zestimate/valuationRange/low")))
+  price_high <- as.numeric(xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//comp/zestimate/valuationRange/high")))
+  prices <- c(price_low,price_high)
+
 
   z_df <- dplyr::data_frame(zpid,bedrooms,bathrooms,year,size,lot_size,value,rent)
   return(z_df)
