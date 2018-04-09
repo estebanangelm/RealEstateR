4b3ca210d8ee1acc5cd103e0bc60761c62bdbeaf
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-04-08 22:16:16 -0700

    Create comparisons scripts file

---
+++ R/comparisons.R
@@ -0,0 +1,62 @@
+#' GetSearchResults Response
+#'
+#' @description Retrieves GetSearchResults API response.
+#'
+#' @param address Street address of interest as a string
+#' (e.g., '2144+Bigelow+Ave')
+#'
+#' @param citystatezip either the city and state or ZIP code
+#' of the street address (e.g., 'Seattle%2C+WA')
+#'
+#' @return API response
+#'
+#' @export
+get_search_results <- function(address, citystatezip) {
+  zwsid <- getOption("ZWSID")
+  url_search <- 'http://www.zillow.com/webservice/GetSearchResults.htm?'
+  result <- httr::GET(url = paste0(url_search, 'zws-id=', zwsid, '&address=', address, '&citystatezip=', citystatezip))
+}
+
+#' Get Zillow Property ID
+#'
+#' @description Retrieves the Zillow Property ID (zpid) for a house
+#' from the GetSearchResults API.
+#'
+#' @param address Street address of interest as a string
+#' (e.g., '2144+Bigelow+Ave')
+#'
+#' @param citystatezip either the city and state or ZIP code
+#' of the street address (e.g., 'Seattle%2C+WA')
+#'
+#' @return zpid as a string
+#'
+#' @export
+get_zpid <- function(address, citystatezip) {
+  zwsid <- getOption("ZWSID")
+  url_search <- 'http://www.zillow.com/webservice/GetSearchResults.htm?'
+  result <- httr::GET(url = paste0(url_search, 'zws-id=', zwsid, '&address=', address, '&citystatezip=', citystatezip))
+  search_xml <- xml2::read_xml(httr::content(result, "text"))
+  zpid <- xml2::xml_text(xml2::xml_find_all(search_xml, ".//zpid"))
+  return(zpid)
+}
+
+#' Get Zestimate
+#'
+#' @description Retrives zestimate information for a specified zpid.
+#'
+#' @param zpid property ID
+#'
+#' @return Zestimate of house (in USD)
+#'
+#' @export
+get_zestimate <- function(zpid) {
+  zwsid <- getOption("ZWSID")
+  url_zillow <- 'http://www.zillow.com/webservice/GetZestimate.htm?'
+  result <- httr::GET(url = paste0(url_zillow,'zws-id=', zwsid, '&zpid=', zpid))
+  zillow_xml <- xml2::read_xml(httr::content(result, "text"))
+  zestimate <- xml2::xml_text(xml2::xml_find_all(zillow_xml, ".//zestimate/amount"))
+  currency <- xml2::xml_attr(xml2::xml_find_all(zillow_xml, ".//zestimate/amount"), "currency")
+  return(zestimate)
+}
+
+
