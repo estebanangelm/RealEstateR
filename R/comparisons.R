45eb317d5245041ccad2e27b75ead2336c90d0c4
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-04-08 22:19:11 -0700

    Create URL for get

--- R/comparisons.R
+++ R/comparisons.R
@@ -3,4 +3,8 @@ get_comp_df <- function(zpid,count){
   if (count > 25){
     count = 25
   }
+  url_zillow <- 'http://www.zillow.com/webservice/GetDeepComps.htm?'
+  url_zillow <- paste0(url_zillow,'zws-id=', zwsid, '&zpid=', zpid,'&count=',count,'&rentzestimate=true')
+  result <- httr::GET(url_zillow)
+  zillow_xml <- xml2::read_xml(httr::content(result, "text"))
 }
