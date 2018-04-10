7abcfda35b1d4a56b2b2206a4d99e9289c0a66f6
Ha Dinh <dinhhn.ubc@gmail.com>
2018-04-10 12:01:15 -0700

    First draft. Test if it output correct number of columns and column names.

---
+++ tests/testthat/test_get_screenname.R
@@ -0,0 +1,42 @@
+context("reviews_get_screenname.R")
+
+# -----------------------------------------------------------------------------
+# Setup
+# -----------------------------------------------------------------------------
+
+library(tidyverse)
+library(xml2)
+library(jsonlite)
+library(rvest)
+library(stringr)
+library(testthat)
+
+zwsid <- function() {
+  val <- Sys.getenv("ZWSID")
+  if (identical(val, "")) {
+    stop("`ZWSID` env var has not been set")
+  }
+  val
+}
+zwsid <- zwsid()
+
+# -----------------------------------------------------------------------------
+# reviews_get_screennames
+# -----------------------------------------------------------------------------
+
+## test expected output
+response <- reviews_get_screennames("Cincinnati", "OH")
+
+test_that("reviews_get_screennames() outputs a dataframe given correct combination of city and state", {
+
+  # expect a dataframe with 5 columns
+  expect_output(str(response), "5 variables", ignore.case = TRUE,
+                          "There should be 5 columns in the dataframe")
+
+  # expect a dataframe with column `name`, `screenname`, `phone`, `city`, `state`
+  expect_output (str(response), "$ name", fixed = TRUE)
+  expect_output (str(response), "$ screenname", fixed = TRUE)
+  expect_output (str(response), "$ phone", fixed = TRUE)
+  expect_output (str(response), "$ city", fixed = TRUE)
+  expect_output (str(response), "$ state", fixed = TRUE)
+})
