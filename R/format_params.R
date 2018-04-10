300d220ae654de57904dec837eb87901b534e895
Jill Cates <cates.jill@gmail.com>
2018-04-09 21:50:55 -0700

    Move format functions to separate file

---
+++ R/format_params.R
@@ -0,0 +1,27 @@
+#' Format Address
+#'
+#' @description Converts the property address to the query param format
+#'
+#' @param address Address as a string (e.g., '2144 Bigelow Ave')
+#'
+#' @return proper format of address as a string (e.g., '2144+Bigelow+Ave')
+#'
+#' @export
+format_address <- function(address) {
+  formatted_address <- gsub("\\s", "+", address)
+  return(formatted_address)
+}
+
+#' Format City and State
+#'
+#' @description Format city and state query parameter
+#'
+#' @param city City of property (e.g., 'Seattle')
+#'
+#' @param state State of property. Can be abbreviated (e.g., 'WA' for Washington)
+#'
+#' @return proper format of city/state as a string (e.g., 'Seattle%2C+WA')
+#' @export
+format_citystate <- function(city, state) {
+  paste0(city,'%2C+',state)
+}
