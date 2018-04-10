e61eaf370fd4c158e6ab38c6739d0b32b326babc
Jill Cates <cates.jill@gmail.com>
2018-04-09 22:21:45 -0700

    Add test for format_params

---
+++ tests/testthat/test_format_params.R
@@ -0,0 +1,23 @@
+context("format_params.R")
+
+test_that("format_address() converts address to appropriate format", {
+  address <- '2144 Bigelow Ave'
+  output <- format_address(address)
+  expect_equal(output, '2144+Bigelow+Ave')
+})
+
+test_that("format_citystate() converts city and state to appropriate format", {
+  city <- "Miami"
+  state <- "FL"
+  output <- format_citystate(city,state)
+  expect_equal(output, 'Miami%2C+FL')
+})
+
+test_that("format_citystate() converts (multi-word) city
+          and state to appropriate format", {
+  city <- "New York"
+  state <- "NY"
+  output <- format_citystate(city,state)
+  expect_equal(output, 'New+York%2C+NY')
+})
+
