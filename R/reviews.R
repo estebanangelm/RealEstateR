34142cd740bc30d12a0ffcd1911b1fee081cb7c3
Ha Dinh <dinhhn.ubc@gmail.com>
2018-04-12 12:00:55 -0700

    Finished remodifying reviews.R

--- R/reviews.R
+++ R/reviews.R
@@ -11,7 +11,7 @@
 #' @return A dataframe that includes details of agents, and their star reviews.
 #'
 #' @examples
-#' \dontrun{screennames <- c("mwalley0", "pamelarporter", "klamping4", "brettkeppler", "Cincysrealtor")
+#' \dontrun{screennames <- c("mwalley0", "pamelarporter", "klamping4", "Cincysrealtor")
 #' reviews(screennames)}
 #'
 #' @export
@@ -22,21 +22,34 @@ reviews <- function(screennames){
 
   df <- NULL
 
+  # check conditions of screenname: a character input, with length 5 at most
+  if (is.character(screennames) != TRUE){
+    stop("Expect screennames input to be character.")
+  }
+
+  if (length(screennames) > 5){
+    stop("Expect at most 5 screennames.")
+  }
+
   # get all info of agents in a location
-  for (s in screenname){
+  for (s in screennames){
     uri <- paste0("http://www.zillow.com/webservice/ProReviews.htm?zws-id=", zwsid, "&screenname=", s, "&output=json")
 
     content_json <- httr::GET(uri) %>% httr::content("text")
+
     content <- jsonlite::fromJSON(content_json)
 
-    name <- content$response$results$proInfo$name
-    screenname <- content$response$results$screenname
+    status <- content$message$text
+
     title <- content$response$results$proInfo$title
     businessName <- content$response$results$proInfo$businessName
     businessAddress <- content$response$results$proInfo$address
     phone <- content$response$results$proInfo$phone
+
+    name <- content$response$results$proInfo$name
+    screenname <- content$response$results$screenname
     specialties <- content$response$results$proInfo$specialties
-    # serviceArea <- content$response$results$proInfo$serviceAreas
+    serviceArea <- content$response$results$proInfo$serviceAreas
     recentSaleCount <- content$response$results$proInfo$recentSaleCount
     reviewCount <- content$response$results$proInfo$reviewCount
     localknowledgeRating <- content$response$results$proInfo$localknowledgeRating
@@ -44,10 +57,10 @@ reviews <- function(screennames){
     responsivenessRating <- content$response$results$proInfo$responsivenessRating
     negotiationskillsRating <- content$response$results$proInfo$negotiationskillsRating
 
-    df <-  rbind(df, data.frame(name, screenname, title,
-                                businessName, businessAddress, phone,
-                                specialties, recentSaleCount, reviewCount,
-                                localknowledgeRating, processexpertiseRating, responsivenessRating, negotiationskillsRating))
+    df <-  rbind(df, tibble::tibble(status, name, screenname, title,
+                                    businessName, businessAddress, phone,
+                                    specialties, serviceArea =  list(serviceArea), recentSaleCount, reviewCount,
+                                    localknowledgeRating, processexpertiseRating, responsivenessRating, negotiationskillsRating))
 
   }
 
